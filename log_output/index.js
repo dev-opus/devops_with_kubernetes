431fd896f6157086551719e23ae1a5c08e12506a
victor orlunda <vicorlunda@gmail.com>
2026-01-02 13:30:58 +0100

    extend log_ouput for exercise 1.7

--- log_output/index.js
+++ log_output/index.js
@@ -1,18 +1,35 @@
 import { randomUUID } from 'node:crypto';
+import { createServer } from 'node:http';
 
-start();
+const server = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  const string = generateString();
+  res.end(prefixWithDate(generateString()) + '\n');
+});
 
-function start() {
-  setInterval(() => {
-    printWithDate(generateString());
-  }, 5000);
-}
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
+
+// start();
+
+// function start() {
+//   setInterval(() => {
+//     printWithDate(generateString());
+//   }, 5000);
+// }
 
 function generateString() {
   return randomUUID();
 }
 
-function printWithDate(string) {
+// function printWithDate(string) {
+//   const isoDateString = new Date().toISOString();
+//   console.log(`${isoDateString}: ${string}`);
+// }
+
+function prefixWithDate(string) {
   const isoDateString = new Date().toISOString();
-  console.log(`${isoDateString}: ${string}`);
+  return `${isoDateString}: ${string}`;
 }
