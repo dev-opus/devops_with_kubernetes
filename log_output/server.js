1d32ac15baf5abe84ee3a76511d514e27ef2a6e2
victor orlunda <vicorlunda@gmail.com>
2026-01-06 16:03:25 +0100

    for exercise 2.1

--- log_output/server.js
+++ log_output/server.js
@@ -1,22 +1,27 @@
+import dotenv from 'dotenv';
 import path from 'node:path';
 import { existsSync } from 'node:fs';
 import { createServer } from 'node:http';
 import { fileURLToPath } from 'node:url';
 import { mkdir, readFile } from 'node:fs/promises';
 
+dotenv.config();
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const pingpongSvc = process.env.PINGPONG_SVC;
+
 const server = createServer(async (req, res) => {
   const content = await getContentFromFile();
-  const pongs = await getPongsFromFile();
+  const pongs = await getPongsViaNetwork();
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end(content + `Ping / Pong: ${pongs}`);
 });
 
 server.listen(3001, () => {
-  console.log('Server running on port 3000');
+  console.log('Server running on port 3001');
 });
 
 async function getContentFromFile() {
@@ -35,6 +40,7 @@ async function getContentFromFile() {
   return content;
 }
 
+// Halt volume-based usage
 async function getPongsFromFile() {
   const logsDir = path.join(__dirname, 'logs');
   const filePath = path.join(logsDir, 'pongs.txt');
@@ -45,4 +51,19 @@ async function getPongsFromFile() {
 
   const pongs = await readFile(filePath, { encoding: 'utf8' });
   return pongs;
+}
+
+async function getPongsViaNetwork() {
+  console.log({ pingpongSvc });
+  try {
+    const res = await fetch(`${pingpongSvc}/get-pingpong`);
+    const pongs = await res.text();
+    return pongs;
+  } catch (error) {
+    console.error(
+      'an error occured fetching pongs via the network',
+      error.stack
+    );
+    return 0;
+  }
 }
--- pingpong/index.js
+++ pingpong/index.js
@@ -1,24 +1,35 @@
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { createServer } from 'node:http';
-import { mkdir, writeFile } from 'node:fs/promises';
+import { mkdir, readFile, writeFile } from 'node:fs/promises';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const logsDir = path.join(__dirname, 'logs');
+const filePath = path.join(logsDir, 'pongs.txt');
+
 let counter = 0;
 
 const server = createServer(async (req, res) => {
-  if (req.url === '/pingpong') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end(`pong ${counter}`);
-    counter += 1;
-    await writePongsToFile(counter);
-  } else {
-    res.statusCode = 404;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Page not found');
+  switch (req.url) {
+    case '/pingpong':
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end(`pong ${counter}`);
+      await writePongsToFile(counter);
+      counter += 1;
+      break;
+    case '/get-pingpong':
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      const pongs = await readPongsFromFile(filePath);
+      res.end(`${pongs}`);
+      break;
+    default:
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Page not found');
   }
 });
 
@@ -27,9 +38,11 @@ server.listen(3000, () => {
 });
 
 async function writePongsToFile(counter) {
-  const logsDir = path.join(__dirname, 'logs');
-  const filePath = path.join(logsDir, 'pongs.txt');
-
   await mkdir(logsDir, { recursive: true });
   await writeFile(filePath, counter.toString(), { flag: 'w' });
+}
+
+async function readPongsFromFile(filePath) {
+  const pongs = await readFile(filePath, 'utf8');
+  return pongs;
 }
